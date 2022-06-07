ffb1e686f813baf99a6de50d5925a52b5a075358
saullope <saul.lopez688@std.uni.edu.ni>
2022-06-07 00:11:45 -0600

    Eliminando la carpeta Backend

--- frontend-vue/src/components/CrudEstudiantes.vue
+++ frontend-vue/src/components/CrudEstudiantes.vue
@@ -46,9 +46,7 @@
     </div>
     <!-- modal para agregar estudiantes -->
     <Dialog header="Agregar nuevo estudiante" v-model:visible="displayModal" :style="{width: '50vw'}" :modal="true">
-            <p class="m-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            
             <template #footer>
                 <Button label="Cancelar" icon="pi pi-times" @click="closeModal" class="p-button-text"/>
                 <Button label="Guardar" icon="pi pi-check" @click="saveInfo" autofocus />
@@ -70,7 +68,8 @@ export default {
             messages: [],
             columns:null,
             estudiantes:null,
-
+            nombre: null,
+            apellidos: null,
             dialog: false,
       dialogDelete: false,
    
