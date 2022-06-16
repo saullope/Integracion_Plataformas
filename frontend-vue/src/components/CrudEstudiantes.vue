b15702a6009a2d748c97a655c288756f6316f81b
Moises Torres <moisestorres0121@gmail.com>
2022-06-16 00:32:28 -0600

    eliminar

--- frontend-vue/src/components/CrudEstudiantes.vue
+++ frontend-vue/src/components/CrudEstudiantes.vue
@@ -4,12 +4,13 @@
         <br><br>
         <Toolbar class="mb-4">
             <template #start>
-            <Button label="Nuevo Estudiante" icon="pi pi-plus" class="mr-2" @click="openNew"/>
-         
-               <span class="p-input-icon-left">
-                        <i class="pi pi-search" />
+            <Button label="Nuevo Estudiante" icon="pi pi-plus" class="mr-5" @click="openNew"/>
+         <Button label="Eliminar seleccionados" icon="pi pi-trash" class="p-button-danger mr-5" @click="confirmDeleteSelected" :disabled="!selectedProducts || !selectedProducts.length" />
+               
+               <span class="p-input-icon-left ml-5 " >
+                        <i class="pi pi-search " />
 
-                        <InputText v-model="filters['global'].value" placeholder="Buscar..." />
+                        <InputText v-model="filters['global'].value" placeholder="Buscar... "  />
                  </span>
             
             </template>
@@ -20,8 +21,27 @@
         </Toolbar>
         <!-- cuerpo del data table -->
          
-    
-
+      <Dialog v-model:visible="deleteestudianteDialog" :style="{width: '450px'}" header="Confirmar" :modal="true">
+            <div class="confirmation-content">
+                <i class="pi pi-exclamation-triangle mr-3" style="font-size: 2rem" />
+                <span v-if="datos">¿Deseas eliminar a <b>{{datos.nombre}}</b>?</span>
+            </div>
+            <template #footer>
+                <Button label="No" icon="pi pi-times" class="p-button-text" @click="deleteestudianteDialog = false"/>
+                <Button label="Si" icon="pi pi-check" class="p-button-text" @click="eliminarEstudiantes" />
+            </template>
+        </Dialog>
+        
+        <Dialog v-model:visible="deleteProductsDialog" :style="{width: '580px'}" header="Confirm" :modal="true">
+            <div class="confirmation-content">
+                <i class="pi pi-exclamation-triangle mr-3" style="font-size: 2rem" />
+                <span v-if="datos">¿Deseas eliminar todos los estudiantes seleccionados?</span>
+            </div>
+            <template #footer>
+                <Button label="No" icon="pi pi-times" class="p-button-text" @click="deleteProductsDialog = false"/>
+                <Button label="Yes" icon="pi pi-check" class="p-button-text" @click="deleteSelectedProducts" />
+            </template>
+        </Dialog>
 
 
 
@@ -33,29 +53,36 @@
                         
                     <h5 class="text-left">Nombre</h5>
                     <InputText id="nombreform" v-model.trim="datos.nombre" required="true" autofocus :class="{'p-invalid': submitted && !datos.nombre}" />
-                    <small class="p-error" v-if="submitted && !datos.nombre">Name is required.</small>
+                    <small class="p-error" v-if="submitted && !datos.nombre"><h5 class="text-left">Hace falta el Nombre</h5></small>
                  <div class="field">
                         
                     <h5 class="text-left">Apellido</h5>
                     <InputText id="apellidosform" v-model.trim="datos.apellidos" required="true" autofocus :class="{'p-invalid': submitted && !datos.apellidos}" />
+                    <small class="p-error" v-if="submitted && !datos.apellidos"><h5 class="text-left">Hace falta el Apellido</h5></small>
+
                 </div>
                  <div class="field">
                         
                  <h5 class="text-left">Carnet</h5>
 
                     <InputText id="carnetEstudianteform" v-model.trim="datos.carnetEstudiante" required="true" autofocus :class="{'p-invalid': submitted && !datos.carnetEstudiante}" />
-                </div>
+                    <small class="p-error" v-if="submitted && !datos.carnetEstudiante"><h5 class="text-left">Hace falta el Carnet</h5></small>
+              
+               </div>
                  <div class="field">
                         
                     <h5 class="text-left">Grupo</h5>
 
                   <Dropdown v-model="datos.opcionesgrupos" :options="grupos" :editable="true" optionLabel="label" placeholder="Selecciona un grupo" />
-                </div>
+                <small class="p-error" v-if="submitted && !datos.opcionesgrupos"><h5 class="text-left">Hace falta el grupo</h5></small>
+
+               </div>
                 
                 <div class="field"> 
                       <h5 class="text-left">Departamento</h5>
 
                   <Dropdown v-model="datos.opciondepartamento" :options="ciudades" :editable="true" optionLabel="label" placeholder="Selecciona un departamento" />
+                        <small class="p-error" v-if="submitted && !datos.opciondepartamento"><h5 class="text-left">Hace falta el departamento</h5></small>
 
                  </div>
                  
@@ -70,18 +97,7 @@
             </template>
             
         </Dialog>
-        <Dialog v-model:visible="deleteestudianteDialog" :style="{width: '450px'}" header="Confirm" :modal="true">
-            <div class="confirmation-content">
-                <i class="pi pi-exclamation-triangle mr-3" style="font-size: 2rem" />
-                <span v-if="product">Are you sure you want to delete the selected products?</span>
-            </div>
-            <template #footer>
-                <Button label="No" icon="pi pi-times" class="p-button-text" @click="deleteestudianteDialog = false"/>
-                <Button label="Yes" icon="pi pi-check" class="p-button-text" @click="eliminarEstudiantes(datos)" />
-            </template>
-        </Dialog>
-
-    
+        
 
 
         
@@ -101,9 +117,9 @@
     <Column field="ciudadDepartamento" header="Departamento" :sortable="true"></Column>
  
      <Column header="Opciones" :exportable="false" style="min-width:8rem">
-              <template #body="slotProps">
-               <Button icon="pi pi-pencil" class="p-button-rounded p-button-success mr-2" @click="editProduct(slotProps.datos)" />
-               <Button icon="pi pi-trash" class="p-button-rounded p-button-warning" @click="confirmarborrardialogo" />
+              <template #body="datos">
+               <Button icon="pi pi-pencil" class="p-button-rounded p-button-success mr-2" @click="EditarEstudiantes(datos)" />
+             <!--   <Button icon="pi pi-trash" class="p-button-danger" @click="deleteestudianteDialog = true" />-->
               </template>
      </Column>
 
@@ -134,8 +150,7 @@ export default {
     data(){
         return {
             Estudiant_table: null,
-            originaldata:[],
-
+            deleteProductsDialog:false,
             displayModal: false,
             filters: {},
             messages: [],
@@ -209,57 +224,66 @@ export default {
         exportCSV(){
             this.$refs.dt.exportCSV();
         },
+        deleteSelectedProducts() {
+            this.Estudiant_table = this.Estudiant_table.filter(val => !this.selectedProducts.includes(val));
+            this.deleteProductsDialog = false;
+            this.selectedProducts = null;
+            this.$toast.add({severity:'success', summary: 'Successful', detail: 'Estudiantes eliminados', life: 3000});
+        },
          eliminarEstudiantes() {
-            this.Estudiant_table = this.Estudiant_table.filter(val => val.idestudiante !== this.datos.idestudiante);
+            this.Estudiant_table = this.Estudiant_table.filter(val => val.id !== this.datos.id);
             this.deleteestudianteDialog = false;
             this.datos = {};
-            this.$toast.add({severity:'success', summary: 'Successful', detail: 'Estudiante eliminado', life: 3000});
+            this.$toast.add({severity:'success', summary: 'Listo!', detail: 'Estudiante eliminado', life: 3000});
         },
          confirmarborrardialogo() {
             this.datos = datos;
             this.deleteestudianteDialog = true;
         },
+          confirmDeleteSelected() {
+            this.deleteProductsDialog = true;
+        },
         EditarEstudiantes(datos) {
             this.datos = {...datos};
-            this.productDialog = true;
+            this.estudianteDialog = true;
         },
         guardarestudiante(){
             this.submitted = true;
             if (this.datos.nombre.trim()) {
-                if (this.datos.idestudiante) {
+                if (this.datos.idEstudiante) {
                     this.datos.nombre = this.datos.nombre;
                     this.datos.apellidos = this.datos.apellidos;
                     this.datos.carnetEstudiante = this.datos.carnetEstudiante ? this.datos.correo.carnetEstudiante : 'sin carnet';
                     this.datos.NombreGrupo = this.datos.NombreGrupo;
                     this.datos.ciudadDepartamento = this.datos.ciudadDepartamento;
-                    this.Estudiant_table[this.findIndexById(this.datos.ididestudiante)] = this.datos;
+                    this.Estudiant_table[this.findIndexById(this.datos.idestudiante)] = this.datos;
                     this.$toast.add({severity:'success', summary: 'Successful', detail: 'Estudiante Actualizado', life: 3000});
                 }
                 else {
-                    this.datos.idestudiante = this.createId();
+                    this.datos.idEstudiante = this.createId();
                     this.datos.nombre = this.datos.nombre;
                     this.datos.apellidos = this.datos.apellidos;
                     this.datos.carnetEstudiante = this.datos.carnetEstudiante ;
                     this.datos.NombreGrupo = this.datos.opcionesgrupos.value;
                     this.datos.ciudadDepartamento = this.datos.opciondepartamento.value;
                     this.Estudiant_table.push(this.datos);
-                    this.$toast.add({severity:'success', summary: 'Successful', detail: 'Estudiante Añadido', life: 3000});
+                    this.$toast.add({severity:'success', summary: 'Listo!', detail: 'Estudiante Añadido', life: 3000});
                     this.estudianteDialog = false;                }
 
             }
         },
-           createId() {
-            let idestudiante = '';
-            var chars = '1234567890';
+        createId() {
+            let id = '';
+            var chars = 'ABCDEFGEHIJKLMNOPQRSTUVWXYZ1234567890';
             for ( var i = 0; i < 5; i++ ) {
-                idestudiante += chars.charAt(Math.floor(Math.random() * chars.length));
+                id += chars.charAt(Math.floor(Math.random() * chars.length));
             }
-            return idestudiante;
+            return id;
         },
-         findIndexById(idestudiante) {
+         findIndexById(id) {
             let index = -1;
             for (let i = 0; i < this.Estudiant_table.length; i++) {
-                if (this.Estudiant_table[i].idestudiante === idestudiante) {
+                if (this.Estudiant_table[i].id === id) {
                     index = i;
                     break;
                 }
